db0be62be11cdc57728930617a329f6a960796e0
hysnn <hsynn03@gmail.com>
2020-10-05 08:15:16 +0300

    i have updated hello.java file

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -1,5 +1,8 @@
 public class hello {
     public static void main(String[] args) {
-        System.out.println("hello there");
+        System.out.println("hello class E3");
+        //more changes are coming
+        //i am ready to commit
+
     }
 }
